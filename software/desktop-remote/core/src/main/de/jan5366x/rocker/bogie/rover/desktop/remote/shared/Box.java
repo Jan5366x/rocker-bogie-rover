fb415c7f1a9c20cdce54a12461aff1f79c0b723c
Jan Schwien <jan5366@gmail.com>
2025-02-07 21:49:44 +0100

    Small shape renderer tests, Update run config to singleton start

---
+++ software/desktop-remote/core/src/main/de/jan5366x/rocker/bogie/rover/desktop/remote/shared/Box.java
@@ -0,0 +1,6 @@
+package de.jan5366x.rocker.bogie.rover.desktop.remote.shared;
+
+import com.badlogic.gdx.graphics.Color;
+
+public record Box(float x, float y, float width, float height, float rotation, Color color) {
+}
--- software/desktop-remote/core/src/main/de/jan5366x/rocker/bogie/rover/desktop/remote/shared/screen/MainScreen.java
+++ software/desktop-remote/core/src/main/de/jan5366x/rocker/bogie/rover/desktop/remote/shared/screen/MainScreen.java
@@ -5,17 +5,22 @@ import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import de.jan5366x.rocker.bogie.rover.desktop.remote.shared.Box;
 import lombok.Getter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
 
 public class MainScreen implements InputProcessor, Screen {
     private static final Logger LOGGER = LogManager.getLogger(MainScreen.class);
@@ -26,21 +31,23 @@ public class MainScreen implements InputProcessor, Screen {
     @Getter
     private final Viewport viewport;
     @Getter
-    private Skin skin;
+    private final Skin skin;
     private SpriteBatch batch;
     private Texture background;
-
+    private final OrthographicCamera camera;
+    private final ShapeRenderer shapeRenderer;
 
     private final InputMultiplexer inputMultiplexer = new InputMultiplexer();
 
+    private final ArrayList<Box> boxes = new ArrayList<>();
 
     public MainScreen() {
 
         viewport = new ScreenViewport();
         stage = new Stage(viewport);
-
         skin = new Skin(Gdx.files.internal("ui/game-ui.json"));
-
+        shapeRenderer = new ShapeRenderer();
+        camera = new OrthographicCamera();
 
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("ui/fonts/cascadia/CaskaydiaCoveNerdFont-Regular.ttf"));
         FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
@@ -75,6 +82,16 @@ public class MainScreen implements InputProcessor, Screen {
         inputMultiplexer.addProcessor(stage);
         inputMultiplexer.addProcessor(this);
         Gdx.input.setInputProcessor(inputMultiplexer);
+
+
+        // Test boxes
+        boxes.add(new Box(100.0F, 100.0F, 30.0F, 60.0F, -30.0F, Color.CYAN));
+        boxes.add(new Box(100.0F, 250.0F, 30.0F, 60.0F, 0.0F, Color.CYAN));
+        boxes.add(new Box(100.0F, 400.0F, 30.0F, 60.0F, 30.0F, Color.CYAN));
+        boxes.add(new Box(350.0F, 100.0F, 30.0F, 60.0F, -30.0F, Color.CYAN));
+        boxes.add(new Box(350.0F, 250.0F, 30.0F, 60.0F, 0.0F, Color.CYAN));
+        boxes.add(new Box(350.0F, 400.0F, 30.0F, 60.0F, 30.0F, Color.CYAN));
+        boxes.add(new Box(150.0F, 140.0F, 180.0F, 300.0F, 0.0F, Color.CYAN));
     }
 
     @Override
@@ -89,10 +106,20 @@ public class MainScreen implements InputProcessor, Screen {
         font.draw(batch, Gdx.graphics.getFramesPerSecond() + " FPS; Delta " + delta * 1000 + "ms", 0, viewport.getScreenHeight());
         batch.end();
 
+        for (Box box : boxes) {
+            shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+            shapeRenderer.setColor(box.color());
+            // public void rect(float x, float y, float originX, float originY, float width, float height, float scaleX, float scaleY, float degrees) {
+            shapeRenderer.rect(box.x(), box.y(), box.width() / 2.0F, box.height() / 2.0F, box.width(), box.height(), 1.0F, 1.0F, box.rotation());
+            shapeRenderer.end();
+        }
 
 
         stage.act();
         stage.draw();
+
+
+
     }
 
     private void createBackground() {
@@ -127,6 +154,7 @@ public class MainScreen implements InputProcessor, Screen {
         stage.dispose();
         skin.dispose();
         font.dispose();
+        shapeRenderer.dispose();
     }
 
 
