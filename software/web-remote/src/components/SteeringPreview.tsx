f10834e27e6f04b217873acc36bfd88e77bc149b
Jan Schwien <jan5366@gmail.com>
2025-08-17 13:57:50 +0200

    Add 3D model rendering to `SteeringPreview` using Three.js and update dependencies

--- software/web-remote/src/components/SteeringPreview.tsx
+++ software/web-remote/src/components/SteeringPreview.tsx
@@ -1,12 +1,31 @@
-import React from "react";
-import {Typography} from "@mui/material";
+import React, {Suspense} from "react";
+import {Box} from "@mui/material";
 import "./Remote.css";
+import {Canvas} from "@react-three/fiber"
+import {useGLTF} from "@react-three/drei"
+
+
+function Model() {
+    const modelPath = new URL("/rover-body.glb", import.meta.url).href
+    const gltf = useGLTF(modelPath)
+    // eslint-disable-next-line react/no-unknown-property
+    return <primitive object={gltf.scene} />
+}
+
 
 function SteeringPreview() {
+
     return (
-        <>
-           <Typography>Steering Preview</Typography>
-        </>
+        <Box width={600} height={600}>
+            <Canvas
+                camera={{ position: [12, 11, 0], fov: 19.5, near: 1, far: 20 }}
+            >
+                <ambientLight />
+                <Suspense>
+                    <Model />
+                </Suspense>
+            </Canvas>
+        </Box>
     );
 }
 
